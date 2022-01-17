66ab408e0c5b61be37d2fac2f446aa57d96be8ed
Vinay2603 <91530880+Vinay2603@users.noreply.github.com>
2022-01-17 16:30:47 +0530

    sdd

--- src/component/Applyjob.jsx
+++ src/component/Applyjob.jsx
@@ -33,23 +33,32 @@ const Applyjob =()=>{
 
     return<div>
         <h1>Applyjob</h1>
-         {mainjobs.map((el)=> 
+         {mainjobs?.map((el, i)=> 
          
-         <div style={{
+         <div key={i} style={{
              border : "1px solid black",
              padding : "20px",
 
          }}>
              
-             <h3>Job Title : {el.title}</h3>
-             <h5>Comapny Name : {el.name}</h5>
-             <h5>Salary : {el.Salary} LPA</h5>
-             <h5>Job Description : {el.description} </h5>
-             <h5>Location : {el.location}</h5>
+             <h3>Job Title : {el?.title}</h3>
+             <h5>Comapny Name : {el?.name}</h5>
+             <h5>Salary : {el?.Salary} LPA</h5>
+             <h5>Job Description : {el?.description} </h5>
+             <h5>Location : {el?.location}</h5>
              <button  onClick={()=>{
-                 axios.put('http://localhost:3001/jobs', {...el , status : !el.status  })
+                 axios.patch('http://localhost:3001/jobs', {
+                    name : el?.name,
+                    title: el?.title,
+                    status: !el?.status,
+                    Salary : el?.Salary,
+                    description: el?.description,
+                    location : el?.location,
+                    id: el?.id
+                 })
                   .then(function (response) {
                     console.log(response);
+                    getjobs()
                   })
                   .catch(function (error) {
                     console.log(error);
@@ -59,7 +68,7 @@ const Applyjob =()=>{
                   
                   
                  
-              >{!el.status ? "APPLY FOR JOB" : "APPLIED ALREADY"} </button>
+              >{!el?.status ? "APPLY FOR JOB" : "APPLIED ALREADY"} </button>
          </div>
          //company name, job title, salary range, job description, location,
          
