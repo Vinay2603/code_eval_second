9bbbfd5b0a5d6062fa9b60ded451b51734b355e3
Vinay2603 <91530880+Vinay2603@users.noreply.github.com>
2022-01-17 16:23:57 +0530

    add

--- src/component/Applyjob.jsx
+++ src/component/Applyjob.jsx
@@ -1,7 +1,70 @@
-
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+const axios = require('axios');
 const Applyjob =()=>{
+
+     const [mainjobs ,setMainjob ] = useState(null)
+
+    const {loading ,jobs,error } = useSelector((state)=>({
+        loading : state.loading,
+        jobs : state.jobs ,
+        error : state.error
+    }))
+
+    const dispatch = useDispatch()
+
+    const getjobs =()=>{
+        axios.get('http://localhost:3001/jobs')
+            .then(function (response) {
+          // handle success
+             console.log("res", response.data);
+             setMainjob(response.data)
+              })
+             .catch(function (error) {
+             // handle error
+            console.log(error);
+            })
+      }
+
+      useEffect(()=>{
+          getjobs()
+      },[])
+
+
     return<div>
         <h1>Applyjob</h1>
+         {mainjobs.map((el)=> 
+         
+         <div style={{
+             border : "1px solid black",
+             padding : "20px",
+
+         }}>
+             
+             <h3>Job Title : {el.title}</h3>
+             <h5>Comapny Name : {el.name}</h5>
+             <h5>Salary : {el.Salary} LPA</h5>
+             <h5>Job Description : {el.description} </h5>
+             <h5>Location : {el.location}</h5>
+             <button  onClick={()=>{
+                 axios.put('http://localhost:3001/jobs', {...el , status : !el.status  })
+                  .then(function (response) {
+                    console.log(response);
+                  })
+                  .catch(function (error) {
+                    console.log(error);
+                  });
+             }}
+                 
+                  
+                  
+                 
+              >{!el.status ? "APPLY FOR JOB" : "APPLIED ALREADY"} </button>
+         </div>
+         //company name, job title, salary range, job description, location,
+         
+         )}
+          
     </div>
 }
 
