d9a299262bb2b97713740c02784f238466f66121
Vinay2603 <91530880+Vinay2603@users.noreply.github.com>
2022-01-17 17:28:57 +0530

    add

--- src/component/Applyjob.jsx
+++ src/component/Applyjob.jsx
@@ -71,13 +71,13 @@ const Applyjob =()=>{
             
          }}>
              
-             <h3>Job Title : {el?.title}</h3>
+             <h2>Job Title : {el?.title}</h2>
              <h5>Comapny Name : {el?.name}</h5>
              <h5>Salary : {el?.Salary} LPA</h5>
              <h5>Job Description : {el?.description} </h5>
              <h5>Location : {el?.location}</h5>
              <button  onClick={()=>{
-                 axios.patch('http://localhost:3001/jobs', {
+                 axios.put('http://localhost:3001/jobs', {
                     name : el?.name,
                     title: el?.title,
                     status: !el?.status,
@@ -93,12 +93,14 @@ const Applyjob =()=>{
                   .catch(function (error) {
                     console.log(error);
                   });
+                
+
              }}
                  
                   
                   
                  
-              >{!el?.status ? "APPLY FOR JOB" : "APPLIED ALREADY"} </button>
+              >{!el?.id %2 === 0  ? "APPLY FOR JOB" : "APPLIED ALREADY"} </button>
          </div>
          //company name, job title, salary range, job description, location,
          
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,7 +3,11 @@
 
 const Home =()=>{
     return<div>
-        <h1>Welcome</h1>
+        <h1>Welcome to the job portal</h1>
+        <div>
+        <img src="http://dpnsee.org/wp-content/uploads/2020/01/Job-vacancy.jpg" alt="vacancy" />
+        </div>
+      
     </div>
 }
 
